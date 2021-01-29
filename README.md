71d66c3f74144b8c3c215fc09f221bae890d6f7f
viktoriya maslennikova <libikvikaa@yandex.ru>
2021-01-29 19:49:08 +0500

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/63lbrhhdckow9sdu?svg=true)](https://ci.appveyor.com/project/ViktoriaMasl/appveyor)
---
+++ src/test/java/ru/netology/rest/MobileBankApiTest1.java
@@ -0,0 +1,26 @@
+package ru.netology.rest;
+
+import org.hamcrest.core.IsEqual;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class MobileBankApiTest1 {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some info") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", IsEqual.equalTo("some book"))
+        ;
+    }
+}
